e4e06479cde0068448a9a092fd54efa5719294fe
SemTheLegend <semmathe@gmail.com>
2023-02-13 19:48:55 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    first_name = "Sem"
+    return render_template('index.html', first_name=first_name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name=name)
+
+
+# Create custom Error Pages:
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=50100, host='localhost')
+
